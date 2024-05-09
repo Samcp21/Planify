77238c475b30b36d15d94984fab90b6dd363e31f
Sam <samcp210@gmail.com>
2024-05-09 18:34:19 -0500

    fix/bug-drag

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -6,28 +6,25 @@ export const todoReducer = (initialState = [], action) => {
       return initialState.filter((todo) => todo.id !== action.payload);
     case "[TODO] Index Todo":
       const { id, index } = action.payload;
-      // Encuentra la tarea que se está moviendo
-      console.log("initialState", initialState);
-      console.log("id", id);
+
       const movingTask = initialState.find((todo) => todo.id === id);
-      console.log("movingTask", movingTask);
 
-      // Si no se encontró la tarea, no hacer nada
       if (!movingTask) return initialState;
-      console.log("initialState", initialState);
-      // Encuentra todas las tareas que necesitan ser ajustadas
-      const tasksToAdjust = initialState.filter((todo) => todo.id !== id);
 
-      // Inserta la tarea movida en su nueva posición
+      const tasksToAdjust = initialState.filter(
+        (todo) => todo.id !== id && todo.idTask == movingTask.idTask
+      );
+      const defaultTasks = initialState.filter(
+        (todo) => todo.idTask != movingTask.idTask
+      );
+      
       tasksToAdjust.splice(index, 0, { ...movingTask, index });
 
-      // Actualiza los índices de las tareas restantes
-      console.log("tasksToAdjust", tasksToAdjust);
       const newState = tasksToAdjust.map((todo, idx) => ({
         ...todo,
         index: idx,
       }));
-
+      newState.push(...defaultTasks);
       return newState;
     case "[TODO] Toggle Todo":
       return initialState.map((todo) => {

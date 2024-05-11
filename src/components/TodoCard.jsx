51adc2acd33bb32690a698558b986c945db1f697
Samir Checcllo <samcp210@gmail.com>
2024-05-11 10:58:52 -0500

    fix/text

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,22 +1,22 @@
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { TodoOptionCard } from "./TodoOptionCard";
 
-export const TodoCard = ({ card, onDeleteTodo, onEditTodo,listTasks }) => {
+export const TodoCard = ({ card, onDeleteTodo, onEditTodo, listTasks }) => {
   const { description, title, image } = card;
 
   const { images } = useFetchGifs(title);
 
   return (
-    <div key={card.id} className="card m-3">
+    <div key={card.id} className="card m-3 ">
       <img
         src={image ? image : images?.url}
         className="card-img-top object-fit-cover w-100 "
         alt="Image ToDo"
         style={{ height: "200px" }}
       />
-      <div className="card-body">
+      <div className="card-body text-wrap">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <p className="card-text ">{description}</p>
         <TodoOptionCard
           onDeleteTodo={onDeleteTodo}
           onEditTodo={onEditTodo}
